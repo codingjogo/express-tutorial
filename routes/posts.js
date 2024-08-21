8b59f060cc071f409089f9d36c6a8592f58ec96d
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-21 17:32:15 +0800

    feat : Routes

---
+++ routes/posts.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+let posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'},
+]
+
+// Get all posts
+router.get('/', (req, res) => {
+    const limit = parseInt(req.query.limit);
+
+    // try this out with ?limit=2 and see changes from your posts
+    if (!isNaN(limit) && limit > 0) {
+        // we can chain status code and json methods
+        return res.status(200).json(posts.slice(0, limit));
+    }
+
+    res.json(posts);
+})
+
+// Get single posts
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        return res.status(404).json({message: "Post not Found!"})
+    } 
+
+    res.status(200).json(post);
+})
+
+module.exports = router
