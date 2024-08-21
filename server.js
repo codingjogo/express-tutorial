f1a4d495d8da567c09f55f7613fc3c94ac739f60
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-21 09:44:21 +0800

    refractor : Routing with sendFile, change into Static Web Server

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+// setup static folder
+// [for more information about statis web server](https://www.scaler.com/topics/expressjs-tutorial/express-static/)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
