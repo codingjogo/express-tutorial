57a1d06237fc1b814c481846848774812b27232f
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-21 20:45:00 +0800

    feat : Route for New Post

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import posts from './routes/posts.js';
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 app.use('/api/posts', posts);
 
