ceceef910b1e98e5583e1e5c56b848c64383aa4e
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-22 09:38:17 +0800

    feat : Add logger to API Route GET and path of "/"

--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,13 @@ let posts = [
     {id: 3, title: 'Post Three'},
 ]
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
 // Get all posts
-router.get('/', (req, res) => {
+router.get('/', logger, (req, res) => {
     const limit = parseInt(req.query.limit);
 
     // try this out with ?limit=2 and see changes from your posts
